b23f854498da3c422129d245be5dd8bdd6e7332b
Orkuncuk <123820259+Orkuncuk@users.noreply.github.com>
2023-04-02 01:01:43 +0300

    Update twoSum_array.py
    minik farklılıklarla kendi çözümümü koydum.

--- twoSum_array.py
+++ twoSum_array.py
@@ -6,4 +6,11 @@ def twoSum(self,nums,target):
                 for j in range(i + 1, len(nums)):
                     if nums[i] + nums[j] == target:
                         return [i,j]
-  
+                    
+ #my solution:
+   def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i != j:
+                    if nums[i] + nums[j] == target:
+                        return i,j  
