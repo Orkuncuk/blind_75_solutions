fcfaf21605e2a22143d05a0b442fb6406d6d9cba
Orkuncuk <123820259+Orkuncuk@users.noreply.github.com>
2023-04-04 23:42:19 +0300

    Update best_time_to_buy_and_sell_stock.py

--- best_time_to_buy_and_sell_stock.py
+++ best_time_to_buy_and_sell_stock.py
@@ -11,6 +11,14 @@ def profit(prices):
     return max(profits)
 
 print(profit(prices = prices))
+# yukarıdaki çöümün list comprehension ile yapılmış şekli
+prices = [7,1,5,3,6,4]
+uzunluk = len(prices)
+def p():
+    liste = [prices[j]-prices[i] for i in range(uzunluk) for j in range(i+1,uzunluk)] # list comprehension
+    liste.append(0)
+    return max(liste)
+print(p())
 
 # yukarıdaki çözüm aslında çalışıyor ancak zaman hatasına takılıyor bu yüzden şöyle bir çözüm daha var
 class Solution:
